7d94e15a41d407142cbddbea09f2e2aaad2179a0
Elton Stoneman <elton@sixeyed.com>
2019-11-21 08:29:26 +0000

    Fix build

--- services/eshop/assets/src/Web/Program.cs
+++ services/eshop/assets/src/Web/Program.cs
@@ -46,7 +46,7 @@ namespace Microsoft.eShopWeb.Web
                 {
                     config.AddJsonFile("appsettings.json")
                           .AddEnvironmentVariables()
-                          .AddJsonFile("config/db-connection-string", optional: true))
+                          .AddJsonFile("config/db-connection-string", optional: true)
                           .AddJsonFile("/run/secrets/app/config/db-connection-string", optional: true);
                 })
                 .ConfigureLogging((hostingContext, logging) =>
